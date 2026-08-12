777f16dc960463cf094a95da15445ce558b43705
PavelLange <pavel72lange72@gmail.com>
2026-08-12 13:57:48 +0300

    Create front page layout

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($uri) {
+
+    case '/':
+        require __DIR__ . '/../partials/header.php';
+        require __DIR__ . '/../views/front.php';
+        require __DIR__ . '/../partials/footer.php';
+        break;
+
+    case '/new-character':
+        require __DIR__ . '/../partials/header.php';
+        require __DIR__ . '/../views/new_character.php';
+        require __DIR__ . '/../partials/footer.php';
+        break;
+
+    default:
+        http_response_code(404);
+
+        require __DIR__ . '/../partials/header.php';
+
+        echo '<main class="container">';
+        echo '<h1>404 - Page not found</h1>';
+        echo '<p>The page you are looking for does not exist.</p>';
+        echo '</main>';
+
+        require __DIR__ . '/../partials/footer.php';
+        break;
+}
