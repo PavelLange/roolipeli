a8617ada3430e915634a3dc0e0d65ed2f02be9a3
PavelLange <pavel72lange72@gmail.com>
2026-08-14 10:56:39 +0300

    Connect frontend pages and routing

--- public/index.php
+++ public/index.php
@@ -10,11 +10,34 @@ switch ($uri) {
         require __DIR__ . '/../partials/footer.php';
         break;
 
+
+    case '/login':
+        require __DIR__ . '/../partials/header.php';
+        require __DIR__ . '/../views/login.php';
+        require __DIR__ . '/../partials/footer.php';
+        break;
+
+
+    case '/register':
+        require __DIR__ . '/../partials/header.php';
+        require __DIR__ . '/../views/register.php';
+        require __DIR__ . '/../partials/footer.php';
+        break;
+
+
+    case '/campaigns':
+        require __DIR__ . '/../partials/header.php';
+        require __DIR__ . '/../views/campaign.php';
+        require __DIR__ . '/../partials/footer.php';
+        break;
+
+
     case '/new-character':
         require __DIR__ . '/../partials/header.php';
         require __DIR__ . '/../views/new_character.php';
         require __DIR__ . '/../partials/footer.php';
         break;
+
 
     default:
         http_response_code(404);
